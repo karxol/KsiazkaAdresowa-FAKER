b42a1658ecd3711c1633f80e438bae12147dba7d
karxol <107922263+karxol@users.noreply.github.com>
2022-08-22 22:10:59 +0200

    Update 7.1.py

--- 7.1.py
+++ 7.1.py
@@ -1,16 +1,29 @@
-class Tel:
-    def __init__(self,imie, nazwisko, firma, stanowisko,email):
-        self.imie = imie
-        self.nazwisko = nazwisko
-        self.firma = firma
-        self.stanowisko = stanowisko
-        self.email = email
+from faker import Faker
+fake = Faker("pl_PL")
 
-KarolKondracki = Tel(imie='Karol',nazwisko = 'kondracki',firma='BAS',stanowisko='Szef',email="karxol@o2.pl")
-KarolinaKondracki = Tel(imie='Karolina',nazwisko = 'kondracki',firma='BAS',stanowisko='kierownik',email="kierownik@o2.pl")
-AdamKondracki = Tel(imie='Adam',nazwisko = 'kondracki',firma='BAS',stanowisko='kierownik',email="kierownik@o2.pl")
-MichalKondracki = Tel(imie='Michal',nazwisko = 'kondracki',firma='BAS',stanowisko='Szef',email="karxol@o2.pl")
+class BaseContact:
+    def __init__(self,name,surname,priv_phone,email):
+        self.name= name
+        self.surname= surname
+        self.priv_phone= priv_phone
+        self.email= email
+    
+    def contact(self):
+        return f'Wybieram numer {self.priv_phone} i kontaktuje się z {self.name} {self.surname} '
 
-adress_book =[KarolKondracki,KarolinaKondracki,AdamKondracki,MichalKondracki]
-for i in adress_book:
-    print(i.imie,i.nazwisko,i.firma,i.stanowisko,i.email)
+    @property
+    def label_lenght(self):
+        return sum([len(self.first_name), len(self.last_name)])
+
+class BusinessContact(BaseContact):
+    def __init__(self, position_job, business, business_phone,  *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       self.position_job = position_job
+       self.business = business
+       self.business_phone = business_phone
+       
+    def contact_work(self):
+        return f'Wybieram numer do {self.business_phone} i kontaktuje się z {self.name} {self.surname}'
+    
+
+    
