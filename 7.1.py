5b5e34784363e8be24902bed5db31d05ad84d2a7
karxol <107922263+karxol@users.noreply.github.com>
2022-08-23 11:15:36 +0200

    Update 7.1.py

--- 7.1.py
+++ 7.1.py
@@ -24,6 +24,19 @@ class BusinessContact(BaseContact):
        
     def contact_work(self):
         return f'Wybieram numer do {self.business_phone} i kontaktuje się z {self.name} {self.surname}'
+    men = BusinessContact(name=fake.name(), surname=fake.surname(), priv_phone=fake.priv_phone(), email=fake.email(),
+              position_job=fake.position_job(), business=fake.business(), business_phone=fake.business_phone())
+
+
+type_card = int(input("Wybierz typ wizytówki 1.prywatna 2.biznesowa:  "))
+how_many = int(input("Podaj ilosc wizytowek:  "))
     
+for i in range(how_many):
+    if type_card == 1:
+        contact = f"{fake.name()}, {fake.surname()}, {fake.priv_phone()}, {fake.email()}"
+        print(contact)
+    elif type_card == 2:
+        contact = f"{fake.name()}, {fake.surname()}, {fake.business_phone()}, {fake.email()}, {fake.position_job}. {fake.business}"
+        print(contact)
 
     
